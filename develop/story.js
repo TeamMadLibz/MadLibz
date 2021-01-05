999911877b8864d337ac585b96527b015c59d62c
Pro-Code-art <jayme.coder@gmail.com>
2021-01-04 19:50:30 -0800

    random button and story functionality fixed

---
+++ develop/story.js
@@ -0,0 +1,17 @@
+
+function renderSavedStories() {
+    console.log('we are in renderSavedStories')
+    if (localStorage.getItem('stored-stories')) {
+        const savedStories = JSON.parse(localStorage.getItem('stored-stories'));
+        const keysArray = Object.keys(savedStories);
+        keysArray.forEach(function (title) {
+            //print the title
+            console.log(title);
+            //print the story
+            console.log(savedStories[title]);
+        })
+    }
+
+}
+
+renderSavedStories();
